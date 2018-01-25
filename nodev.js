28aa21b9afb752804e962fb8bf9cb8cbe8c38221
Dušan Simić <dusan.simic1810@gmail.com>
2018-01-25 17:39:22 +0100

    Added nodev.js for deployment

---
+++ nodev.js
@@ -0,0 +1,20 @@
+const http = require('http');
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const api = require('./api');
+
+const app = express();
+const server = http.createServer(app);
+
+app.use(bodyParser.json());
+app.use(cors());
+app.use('/api', api);
+
+const PORT = process.env.PORT || 8080;
+server.listen(PORT, '0.0.0.0', err => {
+	if (err) {
+		throw err;
+	}
+	console.log('Listening on port 3000');
+});
