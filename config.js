587d24dc6ca962df79e22763ba7e79b2800790f5
Dušan Simić <dusan.simic1810@gmail.com>
2019-01-05 08:48:32 +0100

    Implemented only location and added minor updates
    Implemented get only location feature for device. This is used on map
    page to get only location of device so it'd be added to the map.
    Also added minor updates regarding the mongo library.

--- config.js
+++ config.js
@@ -1,4 +1,7 @@
 module.exports = {
 	serverUrl: process.env.SERVER_URL || 'mongodb://localhost:27017',
-	dbName: process.env.DB_NAME || 'makerpanel'
+	dbName: process.env.DB_NAME || 'makerpanel',
+	settings: {
+		useNewUrlParser: true
+	}
 };
